dcd590fb909710cfe57a490baaf2a4fa51e00955
Alexis Sukrieh <sukria@gmail.com>
2018-01-15 10:36:26 +0100

    add docker-compose

--- update-system.sh
+++ update-system.sh
@@ -18,6 +18,11 @@ function install_base() {
            $(lsb_release -cs) \
            stable"
 
+    echo "Docker compose"
+    curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    curl -L https://raw.githubusercontent.com/docker/compose/1.18.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
     echo "Updating packages"
     apt-get update
     apt-get install  $(cat custom-packages.list) 
